6c1188c579a9eae5f536719570a26e366191dcae
niberc21 <33437881+niberc21@users.noreply.github.com>
2023-02-11 14:12:10 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="contenedor">
+      <div className="registrate">
+        <h1>Registrate</h1>
+        <form className="formulario" action="">
+          <input
+            type="text"
+            name="usuario"
+            id="usuario"
+            placeholder="Usuario"
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Contraseña"
+          />
+          <input
+            type="password"
+            name="password2"
+            id="password2"
+            placeholder="Repetir Contraseña"
+          />
+          <div className="recaptcha"></div>
+          <button type="submit">Iniciar Sesion</button>
+        </form>
+      </div>
+      <div>
+        <h1>Bienvenido</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
